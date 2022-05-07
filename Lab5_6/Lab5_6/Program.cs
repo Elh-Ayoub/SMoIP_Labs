35c20e1b21652089b4419d4980760c0022c7da2c
Elh-Ayoub <ayoub1998elh@gmail.com>
2022-05-07 16:24:43 +0000

    Added Lab 6

--- Lab5_6/Lab5_6/Program.cs
+++ Lab5_6/Lab5_6/Program.cs
@@ -8,44 +8,60 @@ namespace Lab5_6
         {
             if (args.Length != 3)
             {
-                Console.WriteLine("Usage: [algorithm: -s or -b] [Input file path] [Destination path]");
+                Console.WriteLine("Usage: [algorithm: -se/-sd or -be/-bd] [Input file path] [Destination path]");
                 return;
             }
 
             if (args[0] == "-be" || args[0] == "-bd")
             {
                 //Block
-                    Console.Write("Enter key: ");
-                    string ?key = Console.ReadLine();
-                    RC2 rc2 = new(key);
-                    if (args[0] == "-be"){
-                        // encryption
-                        Console.WriteLine("Encryption of " + args[1] + " to: " + args[2] + " ...");
-                        string encryptedData = rc2.encrypte(File.ReadAllBytes(args[1]));                        
-                        //Console.WriteLine("Encrypted data: " + encryptedData[0] + " | " +encryptedData[1]);
-                        string filename = Path.GetFileNameWithoutExtension(args[1]);
-                        string destinationFile = args[2] + "\\" + filename + ".bin";
-                        File.WriteAllText(destinationFile, encryptedData);
-                        return;
-                    }
-                    if (args[0] == "-bd")
-                    {
-                        // decryption
-                        string encFile = File.ReadAllText(args[1]);
-                        //Console.WriteLine(BinFile[0] + " | " + BinFile[1]);
-                        
-                        byte[] data = rc2.decrypt(encFile);
-                        //foreach (byte b in data)
-                        //{
-                        //    Console.WriteLine("byte: " + b + " Char: " + (char)b);
-                        // }
-                        File.WriteAllBytes(args[2], data);
-                        return;
-                    }
+                Console.Write("Enter key: ");
+                string ?key = Console.ReadLine();
+                RC2 rc2 = new(key);
+                if (args[0] == "-be"){
+                    // encryption
+                    Console.WriteLine("Encryption of " + args[1] + " to: " + args[2] + " ...");
+                    string encryptedData = rc2.encrypte(File.ReadAllBytes(args[1]));
+                    string filename = Path.GetFileNameWithoutExtension(args[1]);
+                    string destinationFile = args[2] + "\\" + filename + ".bin";
+                    File.WriteAllText(destinationFile, encryptedData);
+                    return;
+                }
+                if (args[0] == "-bd")
+                {
+                    // decryption
+                    string encFile = File.ReadAllText(args[1]);
+                    byte[] data = rc2.decrypt(encFile);
+                    File.WriteAllBytes(args[2], data);
+                    return;
+                }
             }
-            else if (args[0] == "-s")
+            else if (args[0] == "-se" || args[0] == "-sd")
             {
                 //Stream
+                Console.Write("Enter key: ");
+                string? key = Console.ReadLine();
+                if (args[0] == "-se")
+                {
+                    //encryption
+                    Console.WriteLine("Encryption of " + args[1] + " to: " + args[2] + " ...");
+                    byte[] content = File.ReadAllBytes(args[1]);
+                    LFG lfg = new(key, content.Length);
+                    byte[] encryptedData = lfg.encrypt(content);
+                    string filename = Path.GetFileNameWithoutExtension(args[1]);
+                    string destinationFile = args[2] + "\\" + filename + ".bin";
+                    File.WriteAllBytes(destinationFile, encryptedData);
+                    return;
+                }
+                if (args[0] == "-sd")
+                {
+                    //decryption
+                    byte[] encFile = File.ReadAllBytes(args[1]);
+                    LFG lfg = new(key, encFile.Length);
+                    byte[] decryptedData = lfg.encrypt(encFile);
+                    File.WriteAllBytes(args[2], decryptedData);
+                    return;
+                }
             }
         }
     }
